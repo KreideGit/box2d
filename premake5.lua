559e6a24660e236541179e623d4723244c9b4ddd
Christian Kreidenhuber <kreidenhuber.c@gmail.com>
2022-05-07 13:23:10 +0200

    [FEATURE] add premake file

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "box2d"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++11"
+    staticruntime "off"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/**.h",
+        "src/**.cpp",
+        "src/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
